ace2da93af2bfce3e446f5342289f219504728d0
AkiraDev <akira.enjiu@gmail.com>
2022-10-27 20:15:45 -0300

    chore: add msw config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  axios.get('/user').then((res) => console.log(res))
   return (
     <div className="App">
       <div>
